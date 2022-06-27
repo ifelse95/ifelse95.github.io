6245a36bd456436f7ad36f816eaad787ef0a00b2
ifelse95 <82993323+ifelse95@users.noreply.github.com>
2022-06-27 16:18:21 -0500

    sbarg site update plus button added

--- project/sbargv2_5/script.js
+++ project/sbargv2_5/script.js
@@ -9,7 +9,7 @@ let sidebarHTML = '<h2>MENU</h2><ul> <li><a href="' + '../../projects.html">Proj
 
 
 //footer
-let footerHTML = '<hr><br><center><p>Established November 12, 2020</p><p><a href="./credits.html">credits</a> | <a href="mailto:ifelse95@gmail.com">contact</a> | <a href="./guestbook.html">guestbook</a></p></center>' ;
+let footerHTML = '<hr><br><center><p>Special thanks to shinyJiggly. Established 2020.</p></center>' ;
 
 
 //element stuff
