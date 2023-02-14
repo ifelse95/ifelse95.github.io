8204363b7966d8b0f95d7f3b03375e1dfb1331ce
ifelse95 <82993323+ifelse95@users.noreply.github.com>
2023-02-13 23:25:16 -0600

    major layout update

--- script/script.js
+++ script/script.js
@@ -1,11 +1,11 @@
 
 //footer
-let footerHTML = '<br><center><p>Established 11.12.2020 | Hosted on <a href="https://wertercatt.com/">wertercatt.com</a></p><p><a href="./credits.html">credits</a> | <a href="mailto:ifelse95@gmail.com">contact</a> | <a href="https://ifelse95.123guestbook.com/l">guestbook</a></p></center>' ;
+//let footerHTML = '<center><p>Established 11.12.2020 | Hosted on <a href="https://wertercatt.com/">wertercatt.com</a></p><p><a href="./credits.html">credits</a>|<a href="mailto:ifelse95@gmail.com">contact</a>|<a href="https://ifelse95.123guestbook.com/l">guestbook</a></p></center>' ;
 
 
-if (document.getElementById("footer")) {
-  document.getElementById("footer").innerHTML = footerHTML;
-}
+//if (document.getElementById("footer")) {
+ // document.getElementById("footer").innerHTML = footerHTML;
+//}
 
 var coll = document.getElementsByClassName("collapsible");
 var i;
